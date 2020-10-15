2259796e275c5a9af5d8c0b4240e5071015c53d7
ppopow <pawel.popow@gmail.com>
2020-10-16 00:48:32 +0200

    nowe przyklady

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let name = 'Hans';
+
+name = 'Monika';
+
+let number = 108
+
+let boolean = false;
+
+let array = [1, 2, 3, 108];
+
+let objectH1 = document.querySelector('h1');
+
+objectH1.innerText = 'Kurs Javascript';
+
+console.log(objectH1);
+
